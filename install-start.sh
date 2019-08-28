66945646b596c5612fe7de1d23f3d1df08aa237a
yuanyr <398916756@qq.com>
2019-08-28 13:47:14 +0800

    install-start.sh

--- install-start.sh
+++ install-start.sh
@@ -2,7 +2,16 @@
 
 chmod +x ./scripts/*
 
-cd scripts/ && ./initialize.sh && ./createCA.sh && ./deployKubectl.sh ./deployEtcd.sh \`
-   ./deployFlannel.sh && ./kube-apiserver-nginx.sh && ./deploy-kube-apiserver.sh && \
-   ./deploy-kube-controller-manager.sh && ./deploy-kube-scheduler.sh && \
-   ./deployDocker.sh && ./deployKubelet.sh && ./deploy-kube-proxy.sh
+cd scripts/ && \
+    ./initialize.sh && \
+    ./createCA.sh && \
+    ./deployKubectl.sh && \ 
+    ./deployEtcd.sh && \
+    ./deployFlannel.sh && \
+    ./kube-apiserver-nginx.sh && \
+    ./deploy-kube-apiserver.sh && \
+    ./deploy-kube-controller-manager.sh && \
+    ./deploy-kube-scheduler.sh && \
+    ./deployDocker.sh && \
+    ./deployKubelet.sh && \
+    ./deploy-kube-proxy.sh
